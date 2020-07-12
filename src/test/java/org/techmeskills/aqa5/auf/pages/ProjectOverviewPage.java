7850234d454c81c57f5afebd226cf4ec46c2d68a
lida-belis <bellida88@mail.ru>
2020-07-12 21:01:11 +0300

    ChainOfInvocations_3

--- src/test/java/org/techmeskills/aqa5/auf/pages/ProjectOverviewPage.java
+++ src/test/java/org/techmeskills/aqa5/auf/pages/ProjectOverviewPage.java
@@ -2,11 +2,11 @@ package org.techmeskills.aqa5.auf.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.techmeskills.aqa5.auf.baseEntity.BasePage;
+import org.techmeskills.aqa5.auf.baseEntity.BasePageFactory;
 import org.techmeskills.aqa5.auf.core.BrowsersService;
 import org.techmeskills.aqa5.auf.elements.UIElement;
 
-public class ProjectOverviewPage extends BasePage {
+public class ProjectOverviewPage extends BasePageFactory {
 
     @FindBy(className = "header-menu-item-selected")
     public WebElement headMenuItemSelected;
@@ -21,6 +21,6 @@ public class ProjectOverviewPage extends BasePage {
     }
 
     public boolean isPageOpened() {
-        return new UIElement(browsersService, headMenuItemSelected).getText().equalsIgnoreCase("Overview");
+        return headMenuItemSelected.getText().equalsIgnoreCase("Overview");
     }
 }
